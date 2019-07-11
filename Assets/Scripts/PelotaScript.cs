c1496a236de8408482c7653183e6e62a5d140540
Miguel Angel Falcón Díaz <lsc_miguel_falcon@hotmail.com>
2019-07-10 23:09:03 -0500

    Inicio del proyecto
    Se creo el área de juego y sus limites, la barra del jugador con su collider2D, y la pelota con su collider2D, rigidbody2D, material para el rigidbody2D, y un script para sus comportamientos.

---
+++ Assets/Scripts/PelotaScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelotaScript : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(Vector2.down * 5.0f,ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
