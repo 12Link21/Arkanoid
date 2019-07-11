2eeec5b3711e9555b2f956ee2bba46041651a454
Miguel Angel Falcón Díaz <lsc_miguel_falcon@hotmail.com>
2019-07-11 16:36:56 -0500

    Control del jugador y rebotabilidad de la pelota
    Se agrego un script para poder controlar la "paleta" del jugador, y se agrego codiogo al script de la pelota para que detecte la colision de la pelota con el jugador y pueda aumentar su velocidad

--- Assets/Scripts/PelotaScript.cs
+++ Assets/Scripts/PelotaScript.cs
@@ -9,11 +9,35 @@ public class PelotaScript : MonoBehaviour {
     // Use this for initialization
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.AddForce(Vector2.down * 5.0f,ForceMode2D.Impulse);
+        Invoke("GoBall", 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Vector2 vel;
+            vel.x = (rb2d.velocity.x / 2) + (collision.collider.attachedRigidbody.velocity.x / 3);
+            vel.y = rb2d.velocity.y;
+            rb2d.velocity = vel;
+        }
+    }
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(5,5), ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-5, 5), ForceMode2D.Impulse);
+        }
+    }
 }
